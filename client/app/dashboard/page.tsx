1d545b266123e95ffc426bceebfd9e66b6e2d850
Github11200 <pateljinay@outlook.com>
2024-08-20 23:42:57 -0700

    working on an API route to push embeddings to supabase

--- client/app/dashboard/page.tsx
+++ client/app/dashboard/page.tsx
@@ -2,7 +2,8 @@
 
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
-import { useState } from "react";
+import { useKindeBrowserClient } from "@kinde-oss/kinde-auth-nextjs";
+import { useEffect, useState } from "react";
 
 export default function Dashboard() {
   const [url, setUrl] = useState("");
