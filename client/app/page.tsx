577d5556e2e6673ae15e5b5e0cd8abb792005b2c
NaitikOza <oza.naitik27@gmail.com>
2024-08-22 21:33:34 -0400

    navigation-menu-debug

--- client/app/page.tsx
+++ client/app/page.tsx
@@ -1,12 +1,34 @@
 // page.tsx
+"use client"
 import { AiOutlineSketch } from "react-icons/ai";
 import { Button } from "@/components/ui/button";
-import NavigationMenuDemo from "@/components/navigation-menu";
+import { NavigationMenu, NavigationMenuTrigger, NavigationMenuContent, NavigationMenuItem, NavigationMenuList } from "@/components/navigation-menu";
+
 
 export default function Home() {
   return (
     <div className="relative">
-      <NavigationMenuDemo />
+      <NavigationMenu>
+        <NavigationMenuTrigger>
+          Menu
+        </NavigationMenuTrigger>
+        <NavigationMenuContent>
+          <NavigationMenuList>
+            <NavigationMenuItem>
+              <a href="#home">Home</a>
+            </NavigationMenuItem>
+            <NavigationMenuItem>
+              <a href="#about">About</a>
+            </NavigationMenuItem>
+            <NavigationMenuItem>
+              <a href="#services">Services</a>
+            </NavigationMenuItem>
+            <NavigationMenuItem>
+              <a href="#contact">Contact</a>
+            </NavigationMenuItem>
+          </NavigationMenuList>
+        </NavigationMenuContent>
+      </NavigationMenu>
       <div className="container mx-auto p-4 mt-16">
         <h1 className="text-4xl font-bold text-center mb-8">AI Rate My Professor</h1>
         <ButtonWithIcon />
@@ -23,6 +45,7 @@ function ButtonWithIcon() {
     </Button>
   );
 }
+
 
 
 
