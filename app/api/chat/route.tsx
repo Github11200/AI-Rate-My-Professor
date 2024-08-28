1ab674e07cd7aefc71f5dbe49bd95e32fb8a9e76
Abdelghani Bensalih <111251662+abdelghaniBensalih@users.noreply.github.com>
2024-08-28 17:44:40 +0000

    fixied 404 not found error, now working on auth redirecting

--- app/api/chat/route.tsx
+++ app/api/chat/route.tsx
@@ -6,8 +6,8 @@ import { createClient } from '@supabase/supabase-js';
 const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL as string;
 if (!supabaseUrl) throw new Error(`Expected NEXT_PUBLIC_SUPABASE_URL`);
 
-const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY as string;
-if (!supabaseKey) throw new Error(`Expected NEXT_PUBLIC_SUPABASE_ANON_KEY`);
+const supabaseKey = process.env.SUPABASE_SERVICE_ROLE_KEY as string;
+if (!supabaseKey) throw new Error(`Expected SUPABASE_SERVICE_ROLE_KEY`);
 
 const supabase = createClient(supabaseUrl, supabaseKey);
 
