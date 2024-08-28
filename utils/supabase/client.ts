f957426dd679c81462fc7c73a4be6d34216eb3f6
Abdelghani Bensalih <111251662+abdelghaniBensalih@users.noreply.github.com>
2024-08-28 18:02:40 +0000

    chore: Update Supabase client configuration

--- utils/supabase/client.ts
+++ utils/supabase/client.ts
@@ -1,8 +1,7 @@
-// utils/supabase/client.ts
-import { createBrowserSupabaseClient } from "@supabase/auth-helpers-nextjs";
+import { createBrowserClient } from "@supabase/ssr";
 
 export const createClient = () =>
-  createBrowserSupabaseClient({
-    supabaseUrl: process.env.NEXT_PUBLIC_SUPABASE_URL!,
-    supabaseKey: process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
-  });
+  createBrowserClient(
+    process.env.NEXT_PUBLIC_SUPABASE_URL!,
+    process.env.SUPABASE_SERVICE_ROLE_KEY!
+  );
